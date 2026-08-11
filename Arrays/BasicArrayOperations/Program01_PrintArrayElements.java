7eb7a332e021a0c4c20d8eec9a289a09f8264510
Kothapalli Gayathri <kothapalligayathri147@gmail.com>
2026-08-11 15:14:42 +0530

    Added program for Array Traversal

---
+++ Arrays/BasicArrayOperations/Program01_PrintArrayElements.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Program01_PrintArrayElements
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no of elements: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter elements:");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int j:arr)
+        {
+            System.out.println(j);
+        }
+
+    }
+}
